1b9d7b384892fd83260e838b93920cb6d3f0b8a8
zbC137 <me-bin.zhang@connect.polyu.hk>
2022-09-21 16:01:59 +0800

    update

--- m files/pid_sim.m
+++ m files/pid_sim.m
@@ -1,68 +1,56 @@
+%PID Controller
 close all;
-clc; clear;
+clear; clc;
 
 %% parameters
-dt = 0.1;
-t = 20;
+ts=0.001;
+sys=tf(5.235e005,[1,87.35,1.047e004,0]);
+dsys=c2d(sys,ts,'z');
+[num,den]=tfdata(dsys,'v');
 
-kp = 2.626;
-ki = 0.1;
-kd = 0;
-
-x1 = 0; % x(-1)
-x0 = 0; % x(0)
-xd = 10; % target value
-
-%% data recording
-eData = [];
-xData = [];
-uData = [];
-tData = [];
-
-%% simulation
-x_curr = x0;
-x_last = x1;
-
-e_curr = xd - x_curr;
-e_last = xd - x_last;
-
-e_sum = e_curr*dt;
-e_diff = (e_curr-e_last)/dt;
-
-xData = [xData; x_curr];
-eData = [eData; e_curr];
-
-for i = 0:dt:t
-
-  tData = [tData; i];
-
-  u = kp*e_curr + ki*e_sum + kd*e_diff;
-
-  % control
-  options = odeset('RelTol', 1e-6, 'AbsTol', 1e-6);
-  [~, v] = ode45(@(t, v)dynamics(t, v, u), [i,i+1], x_curr, options);
-  x_last = x_curr;
-  x_curr = v(end);
-
-  %x_llast = x_last;
-  %x_last = x_curr;
-  %x_curr = -3*x_llast - 3*x_last - 1 + u;
-  %x_curr = x_last + u;
-
-  % update
-  e_last = e_curr;
-  e_curr = xd - x_curr;
-  e_sum = e_sum + e_curr*dt;
-  e_diff = (e_curr-e_last)/dt;
-
-  % save data
-  xData = [xData; x_curr];
-  eData = [eData; e_curr];
-  uData = [uData; u];
-
+u_1=0.0;u_2=0.0;u_3=0.0;
+y_1=0.0;y_2=0.0;y_3=0.0;
+x=[0,0,0]';
+error_1=0;
+for k=1:1:2000
+time(k)=k*ts;
+   
+S=1;
+if S==1
+    kp=0.6;ki=0.01;kd=0.01;          
+    yd(k)=1;                       %Step Signal
+elseif S==2
+    kp=0.50;ki=0.001;kd=0.001;          
+    yd(k)=sign(sin(2*2*pi*k*ts));  %Square Wave Signal
+elseif S==3
+    kp=1.5;ki=1.0;kd=0.01;          %Sine Signal
+    yd(k)=0.5*sin(2*2*pi*k*ts);           
 end
 
-%% plotting
-figure(1)
-plot(tData, xData(1:201))
+u(k)=kp*x(1)+kd*x(2)+ki*x(3);   %PID Controller
+%Restricting the output of controller
+if u(k)>=10       
+   u(k)=10;
+end
+if u(k)<=-10
+   u(k)=-10;
+end
+%Linear model
+y(k)=-den(2)*y_1-den(3)*y_2-den(4)*y_3+num(2)*u_1+num(3)*u_2+num(4)*u_3;
 
+error(k)=yd(k)-y(k);
+
+%Return of parameters
+u_3=u_2;u_2=u_1;u_1=u(k);
+y_3=y_2;y_2=y_1;y_1=y(k);
+   
+x(1)=error(k);                %Calculating P
+x(2)=(error(k)-error_1)/ts;   %Calculating D
+x(3)=x(3)+error(k)*ts;        %Calculating I
+
+error_1=error(k);
+end
+figure(1);
+plot(time,yd,'r',time,y,'k:','linewidth',2);
+xlabel('time(s)');ylabel('yd,y');
+legend('Ideal position signal','Position tracking');
